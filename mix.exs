efb34338fe593872e698b58385f848ce8a3d1d2b
Bryan Green <twistedogre@gmail.com>
2026-01-15 10:57:15 -0800

    Version bump

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule NervesSystemRpi4.MixProject do
   use Mix.Project
 
   @github_organization "codestorm1"
-  @app :nerves_system_rpi4_cec
+  @app :nerves_system_rpi4
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
